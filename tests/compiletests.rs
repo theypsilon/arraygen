e58dc0743af0cc3507bdc214c526a676ba86f51d
José Manuel Barroso Galindo <theypsilon@gmail.com>
2021-06-12 04:14:41 +0200

    Restoring compiletest.

--- tests/compiletests.rs
+++ tests/compiletests.rs
@@ -8,12 +8,12 @@ fn run_mode(mode: &'static str) {
     config.mode = mode.parse().expect("Invalid mode");
     config.src_base = PathBuf::from(format!("tests/{}", mode));
     config.target_rustcflags = Some("-L target/debug -L target/debug/deps".to_string());
-    //config.clean_rmeta();
+    config.clean_rmeta();
 
     compiletest::run_tests(&config);
 }
 
 #[test]
 fn compile_test() {
-    //run_mode("compile-fail");
+    run_mode("compile-fail");
 }
